5e7f373947b034f75f0c07d0260aa53deeadaf51
Gustavo Koyanagui <160496975+Gutoo07@users.noreply.github.com>
2025-03-31 09:48:07 -0300

    Classes de persistência

---
+++ Trabalho 1/TrabalhoLabBd/src/main/java/br/edu/labbd/fateczl/TrabalhoLabBd/model/Cliente.java
@@ -0,0 +1,21 @@
+package br.edu.labbd.fateczl.TrabalhoLabBd.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+	private String rg;
+	private String nome;
+	private String telefone;
+	private LocalDate dt_nasc;
+	private String dt_nascStr;
+	private String senha;
+}
